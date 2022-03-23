4cd883428bcf1e363078a7079e4f6e80dabe4131
havanthuong <havanthuong2000@gmail.com>
2022-03-23 21:18:59 +0700

    edit callback-hell file

--- callback-Hell.js
+++ callback-Hell.js
@@ -1,1 +1,16 @@
-function 
+function wakeUp(doSomething) {
+  doSomething();
+}
+function sleep(doSomething) {
+  console.log("sleep");
+  doSomething();
+}
+sleep(
+  wakeUp(
+    sleep(
+      wakeUp(function () {
+        console.log("Wake Up");
+      })
+    )
+  )
+);
